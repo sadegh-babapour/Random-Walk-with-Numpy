32720fc79dbd1a9ed8ee5c636c0fba5889e03bb7
Sadegh Babapour <32153426+sadekbp@users.noreply.github.com>
2017-09-27 00:13:46 -0600

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -1,61 +1,59 @@
+"""This is a simple program to test a random walk without using loops.
+
+a person rolls a special dice with numbers -2,2,-3 and 3 representing neative and positive steps,
+in horizontal and vertical directions respectively."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-aa = np.int_(np.random.choice([1,-1], size=(1,200)))
-bb = np.int_(np.zeros((1,1)))
-cc = np.concatenate((bb, aa), axis = 1)
-jj = np.random.choice([1,-1], size=(1,200))
-mm = np.concatenate((bb, jj), axis = 1)
-tut = np.array([cc, mm])
-stez =  np.cumsum(tut, axis=2)
-
-
-x = stez[0,:]
-y = stez[1,:]
-
-
-aa = np.int_(np.random.choice([-3, 3, 2, -2], size=(1,10)))
-np.where(cc %3 == 0)
-
-bb = np.int_(np.zeros((1,1)))
-cc = np.concatenate((bb, aa), axis = 1)
-nn = np.int_(np.zeros(1))
-
-
-aa = np.int_(np.random.choice([-3, 3, 2, -2], size=(1,15)))
 
 
 
-aa = np.int_(np.random.choice([-3, 3, 2, -2], size=(1,100)))
+# Generating random dice outcomes in an array (15 rollos of dice for this case) :
 
-xp = np.cumsum(aa == 2)
-xp1 = np.cumsum(aa == -2)
+random_choice = np.int_(np.random.choice([-3, 3, 2, -2], size=(1,25)))
 
-xxx = xp - xp1
+# x is the horizontal moves created by the dice:
+
+# x_positive keeps track to walker x location only when the positive outcme of dice happens
+# x_negative keeps track to walker x location only when the negative outcme of dice happens
+
+x_positive = np.cumsum(random_choice == 2) 
+x_negative = np.cumsum(random_choice == -2)
+
+x_location = x_positive - x_negative    # keeps track of walker horizontal location
 
 
-yp = np.cumsum(aa == 3)
+# y is the horizontal moves created by the dice:
+# y_positive keeps track to walker x location only when the positive outcme of dice happens
+# y_negative keeps track to walker x location only when the negative outcme of dice happens
 
-yp1 = np.cumsum(aa == -3)
+y_positive = np.cumsum(random_choice == 3)
 
-yyy = yp - yp1
+y_nagative = np.cumsum(random_choice == -3)
+
+y_location = y_positive - y_nagative    # keeps track of walker vertical location
 
 
+# Zeroth step, an array with only 0 value is created and concatenated to x and y location array to represnt the starting point
+zero_point = np.array([0])
 
-zn = np.array([0])
-vvv = np.concatenate((zn, xxx))
-qqq = np.concatenate((zn, yyy))
+# horizontal & Vertical location based on random choice outcomes starting at (0,0)
 
-print(vvv)
-print(qqq)
+x_array = np.concatenate((zero_point, x_location))
+y_array = np.concatenate((zero_point, y_location))
 
-gg=zip(vvv,qqq)
-kk= list(gg)
+# Here the seperated x and y locations are put together for each step
 
-start =plt.scatter(kk[0][0],kk[0][1], s = 200, label = "Start")
-end = plt.scatter(kk[-1][0],kk[-1][1], color = "r", s=150, label = "End" )
-lines =plt.plot(*zip(*kk), color = "pink")
+step_location=zip(x_array,y_array)
+
+# converted to list to plot the (x,y) pairs
+
+list_steps= list(step_location)
+
+start =plt.scatter(list_steps[0][0],list_steps[0][1], s = 200, label = "Start")
+end = plt.scatter(list_steps[-1][0],list_steps[-1][1], color = "r", s=150, label = "End" )
+lines =plt.plot(*zip(*list_steps), color = "pink")
 plt.legend(loc = "best")
 plt.show()
